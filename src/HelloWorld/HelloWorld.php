a130dea36fa037fb7b5872ceac9b979b0cb67bff
trungtan <trungtanbui@gmail.com>
2014-06-18 14:52:11 +0700

    Update HelloWorld.php

--- src/HelloWorld/HelloWorld.php
+++ src/HelloWorld/HelloWorld.php
@@ -2,7 +2,9 @@
 
 namespace HelloWorld;
 
-class Hello
+//Classname must be the same with filename?
+//How about namespace name? See PRS-0
+class HelloWorld
 {
     public function sayHello()
     {
