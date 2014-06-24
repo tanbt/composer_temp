f58ac12bded1eab737019bf6d790798067abfc88
trungtan <trungtanbui@gmail.com>
2014-06-24 22:30:05 +0700

    /home/tambt/var/github/composer_temp

---
+++ src/MySecondNS/BazSecond.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace MySecondNS;
+
+class BazSecond
+{
+    public function hello()
+    {
+        return 'Hello World, I\'m second NS baz class.!';
+    }
+}
+?>
---
+++ test/MyNamespace/BazTest.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace MyNamespace;
+
+class BazTest
+{
+    public function hello()
+    {
+        return 'Hello World, I\'m the test of baz class.!';
+    }
+}
+?>
